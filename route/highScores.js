7c36d43c46fa286d725eb858e0fef6e49ddd2f5e
Pang Voreh Uddom <uddompang@gmail.com>
2023-05-20 20:57:36 +1000

    'fixes'

--- route/highScores.js
+++ route/highScores.js
@@ -6,10 +6,9 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = (connection) => {
 
-    router.post('/allCompetition', async (req, res) => {
-        const { competitionType } = req.body;
+    router.get('/allCompetition', async (req, res) => {        
+        const query = `SELECT * FROM competition;`
         
-        const query = `SELECT * FROM competition WHERE competitionType = '${competitionType}';`
         try {
             const [results] = await connection.query(query)
             res.status(200).send(results);
@@ -18,8 +17,18 @@ module.exports = (connection) => {
         }
     });
 
-    router.get('/allCategory', async (req, res) => {
-        query = `SELECT * FROM category;`
+    router.post('/allCategory', async (req, res) => {
+        const { categoryID } = req.body;
+        const query = `
+        SELECT category.categoryID, equipmentType.equipmentName, genderClassification.gender, ageClassification.age
+        FROM category
+        JOIN equipmentType ON category.equipmentTypeID = equipmentType.equipmentTypeID
+        JOIN genderClassification ON category.genderClassificationID = genderClassification.genderClassificationID
+        JOIN ageClassification ON category.ageClassificationID = ageClassification.ageClassificationID
+        JOIN competition ON competition.categoryID = category.categoryID
+        WHERE category.categoryID = ${categoryID}
+        `;
+
         try {
             const [results] = await connection.query(query)
             res.status(200).send(results);
@@ -30,32 +39,35 @@ module.exports = (connection) => {
 
 
     router.post('/highscore', async (req, res) => {
-        const {competitionType, categoryID} = req.body;
+        const {categoryID, competitionID } = req.body;
 
         const query = 
         `SELECT
-            archers.archerID,
-            archers.firstName,
-            archers.lastName,
-            competition.categoryID,
-            SUM(score.arrowOne + score.arrowTwo + score.arrowThree + score.arrowFour + score.arrowFive + score.arrowSix) AS TotalScore
- 
-        FROM
-            archers
-        JOIN
-            end ON archers.archerID = end.archerID
-        JOIN
-            round ON end.roundID = round.roundID
-        JOIN
-            score ON end.scoreID = score.scoreID
-        JOIN
-            competition ON round.competitionID = competition.competitionID
-        WHERE
-            competition.competitionType = ${competitionType}
-            AND competition.categoryID = ${categoryID}
-        GROUP BY
-            archers.archerID,
-            competition.categoryID`
+        archers.archerID,
+        archers.firstName,
+        archers.lastName,
+        competition.categoryID,
+        SUM(score.arrowOne + score.arrowTwo + score.arrowThree + score.arrowFour + score.arrowFive + score.arrowSix) AS TotalScore
+    FROM
+        archers
+    JOIN
+        end ON archers.archerID = end.archerID
+    JOIN
+        round ON end.roundID = round.roundID
+    JOIN
+        score ON end.scoreID = score.scoreID
+    JOIN
+        competition ON round.competitionID = competition.competitionID
+    WHERE
+        competition.competitionID = ${competitionID}
+        AND competition.categoryID = ${categoryID}
+    GROUP BY
+        archers.archerID,
+        competition.categoryID
+    ORDER BY
+        TotalScore DESC;
+    
+        `
 
         try {
             const [results] = await connection.query(query)
